c790e8a21f34ec6f043b3a5f4455d8a2be96a336
Omer Hayat <oh2122@nyu.edu>
2025-11-09 16:29:38 +0400

    Pushing setters, constructors and getters in book class

--- book.hpp
+++ book.hpp
@@ -10,15 +10,79 @@ class Book
 	   string title;              // Title of the book
 	   string author;             // Author of the book
 	   string isbn;               // ISBN (International Standard Book Number) of the book
-	    int publication_year;           // Year the book was published
+	   int publication_year;           // Year the book was published
 
 	public:
-	   //Decleare required methods for Book class
+	   //Constructor
+	   Book();
+	   //Parameterized Constructor
+	   Book(string t, string a, string i, int y);
+
+	   //Getter Methods
+	   string getTitle() const;
+	   string getAuthor() const;
+	   string getISBN() const;
+	   int getYear() const;
+
+	   //Setter Methods
+	   void setTitle(string t);
+	   void setAuthor(string a);
+	   void setISBN(string i);
+	   void setYear(int y);
 };
 
  //Define the methods for Book class
+//=============================================
+
+Book::Book() {
+	title = "";
+	author = "";
+	isbn = "";
+	publication_year = 0;
+}
 
 
+Book::Book(string t, string a, string i, int y) {
+	title = t;
+	author = a;
+	isbn = i;
+	publication_year = y;	
+}
+
+
+
+string Book::getTitle() const{
+	return title;
+}
+
+string Book::getAuthor() const{
+	return author;
+}
+
+string Book::getISBN() const{
+	return isbn;
+}
+
+int Book::getYear() const{
+	return publication_year;
+}
+
+
+void Book::setTitle(string t) {
+	title = t;
+}
+
+void Book::setAuthor(string a) {
+	author = a;
+}
+
+void Book::setISBN(string i) {
+	isbn = i;
+}
+
+void Book::setYear(int y) {
+	publication_year = y;
+}
 
 
 //=============================================
