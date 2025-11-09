5290c24fffcda73739d2a6c58c7cdfa89aea24a6
Omer Hayat <oh2122@nyu.edu>
2025-11-09 17:40:38 +0400

    Adding till addChild

--- tree.hpp
+++ tree.hpp
@@ -17,14 +17,14 @@ class Node
 	    unsigned int bookCount;     // Count of books in this Node (Category) and its all subcategories
 	    Node* parent;               // Pointer to the parent Node (nullptr for the root)
 	public:
-	 	Node(string name, Node* parent);
+	 	Node(const string& name, Node* parent);
 		string getName() const;
 		Node* getParent() const;
 		unsigned int getBookCount() const;
 		MyVector<Node*>& getChildren();
 		MyVector<Book*>& getBooks();
 
-		Node* findChildByName(const string& childName);
+		Node* findChildByName(const string& childName) const;
 		Node* addChild(const string& childName);
 		bool removeChildByName(const string& childName);
 
@@ -73,6 +73,50 @@ class Tree
 //==========================================================
 //Define the methods of Node and Tree Class
 
+Node::Node(const string& name, Node* parent) {
+	this->name = name;
+	this->parent = parent;
+	bookCount = 0;
+}
+
+string Node::getName() const{
+	return name;
+}
+
+Node* Node::getParent() const {
+	return parent;
+}
+
+unsigned int Node::getBookCount() const {
+	return bookCount;
+}
+
+MyVector<Node*>& Node::getChildren() {
+    return children;
+}
+
+MyVector<Book*>& Node::getBooks() {
+    return books;
+}
+
+Node* Node::findChildByName(const string& childName) const {
+	for (int i = 0; i < children.size(); i++){
+		if (children[i]->getName() == childName) return children[i];
+	}
+	return nullptr;	
+}
+
+Node* Node::addChild(const string& childName) {
+	Node* exists = findChildByName(childName);
+	if (exists != nullptr){
+		return exists;
+	}
+	else {
+		Node* child = new Node(childName, this);
+		children.push_back(child);
+		return child;
+	}
+}
 
 
 
